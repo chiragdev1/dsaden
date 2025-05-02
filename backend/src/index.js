0e721e6f9f17b835d817b27dae166b4c6154223a
Chirag Arora <cse.chirag.arora@gmail.com>
2025-05-02 20:22:18 +0530

    Added problem routes and some formatting

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+
+import authRoutes from './routes/auth.routes.js'
+import problemRoutes from './routes/problem.routes.js'
+
 
 dotenv.config()
 
+
 const app = express()
 const port = process.env.PORT || 4000
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+   res.send('<h1>Hello guys! Welcome to DSADEN🔥</h1>')
+})
+
+app.use('/api/v1/auth', authRoutes)
+
+app.use('api/v1/problems', problemRoutes)
 
 app.listen(3000, () => {
    console.log(`Server is running on port ${port}`)
