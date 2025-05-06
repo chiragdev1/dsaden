df45c2d86940d569a06db85953d02dfde893bf51
Chirag Arora <cse.chirag.arora@gmail.com>
2025-05-06 22:20:21 +0530

    Added all problem routes

--- backend/src/routes/problem.routes.js
+++ backend/src/routes/problem.routes.js
@@ -1,10 +1,26 @@
 import express from 'express'
+import { authenticateUser, checkAdmin } from '../middlewares/auth.middlewares'
+import {
+   createProblem,
+   getAllProblems,
+   getProblemById,
+   updateProblem,
+   deleteProblem
+} from "../controllers/problem.controllers";
 
 
 const problemRoutes = express.Router()
 
-problemRoutes.post('/create-problem', createProblem)
+problemRoutes.post('/create-problem', authenticateUser, checkAdmin, createProblem)
 
+problemRoutes.get('/get-all-porblems', authenticateUser, getAllProblems)
 
+problemRoutes.get('/get-problem/:id', authenticateUser, getProblemById)
+
+problemRoutes.put('/update-problem/:id', authenticateUser, checkAdmin, updateProblem)
+
+problemRoutes.delete('/delete-problem/:id', authenticateUser, checkAdmin, deleteProblem)
+
+problemRoutes.get('/get-solved-problems', authenticateUser, getAllProblemsSolvedByUser)
 
 export default problemRoutes
