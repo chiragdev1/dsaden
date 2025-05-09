0e0f605fd97226b2b1eb991d1b1b1f2b516e3701
Chirag Arora <cse.chirag.arora@gmail.com>
2025-05-09 21:09:00 +0530

    Added sleep pollBatchResults and submitBatch

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -10,6 +10,38 @@ export const getJudge0LanguageId = (language) => {
    return languageMap[language.toUpperCase()]
 }
 
+const sleep = (time) => new Promise( (resolve, reject) => setTimeout(resolve, time))
+
+
+export const pollBatchResults = async (tokens) => {
+   while(true) {
+      const {data} = await axios.get(`${JUDGE0_API_URL}/submissions/batch`, {
+         params: {
+            tokens: tokens.join(', '),
+            base64_encoded: false,
+
+         }
+      })
+
+      const results = data.submissions
+
+      const areAllDone = results.every(
+         (r) => r.status.id !== 1 && r.status.id !== 2
+      )
+
+      if(areAllDone) return results
+      await sleep(1000) // wait for 1 second before trying again
+   }
+}
+
+
 export const submitBatch = async (submissions) => {
-   const { data } = await axios.post(`${process.env.JUDGE0_API_URL}`);
+   const { data } = await axios.post(
+      `${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`, {
+         submissions
+      }
+   );
+
+   console.log('Submission results: ', data)
+   return data // Array of tokens [ {token}, {token}, {token}]
 }
